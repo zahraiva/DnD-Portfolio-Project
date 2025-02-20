18ec99cec0d9914f807856411e1dd2dfc423d314
Orxan <9418@holbertonstudents.com>
2025-02-20 13:15:05 +0400

    Adding init into the basemodel

--- backend/models/basemodel.py
+++ backend/models/basemodel.py
@@ -1,4 +1,3 @@
-
 from sqlalchemy import Column, DateTime, String
 import uuid
 from datetime import datetime, UTC
@@ -10,3 +9,10 @@ class BaseModel(Base):
     id = Column(String(36), primary_key=True, unique=True, default=lambda: str(uuid.uuid4()))
     created_at = Column(DateTime, default=lambda: datetime.now(UTC), nullable=False)
     updated_at = Column(DateTime, default=lambda: datetime.now(UTC), onupdate=lambda: datetime.now(UTC), nullable=False)
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        if not hasattr(self, "id") or self.id is None:
+            self.id = str(uuid.uuid4())
+        self.created_at = datetime.now(UTC)
+        self.updated_at = datetime.now(UTC)
