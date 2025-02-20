38d385f2acbf94e7226d81462075909e61df12c0
Orxan <9418@holbertonstudents.com>
2025-02-20 13:19:18 +0400

    Change in Character Action Model

--- backend/models/character_action.py
+++ backend/models/character_action.py
@@ -1,3 +1,4 @@
+
 from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.orm import relationship
 from backend.models.basemodel import BaseModel
@@ -5,11 +6,10 @@ from backend.models.basemodel import BaseModel
 class CharacterAction(BaseModel):
     __tablename__ = "character_action"
 
-    game_session_id = Column(ForeignKey("game_session.id"), nullable=False)
-    game_character_id = Column(ForeignKey("game_character.id"), nullable=False)
-    roll_value = Column(Integer, nullable=True)  # Can be nullable if not every action requires a roll
-    action = Column(String, nullable=False)
+    game_session_id = Column(String(36), ForeignKey("game_session.id"), nullable=False)
+    game_character_id = Column(String(36), ForeignKey("game_character.id"), nullable=False)
+    roll_value = Column(Integer, nullable=True)
+    action = Column(String(255), nullable=False)
 
-    # Relationships
     game_session = relationship("GameSession", back_populates="character_actions")
     game_character = relationship("GameCharacter", back_populates="character_actions")
