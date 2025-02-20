c5eb88f9a8ece8d2a680a4c063dad129bb35f1c0
AliiMustafa <aliimustafazada.com>
2025-02-20 15:17:04 +0400

    Change in run.py file

--- run.py
+++ run.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
-from backend.api.dungeon import router as dungeon_router
+from backend.api.auth import router as dungeon_router
+from backend.database import engine, Base
+import uvicorn
+
+def create_tables():
+    Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-
 app.include_router(dungeon_router)
+
+
+
+if __name__ == "__main__":
+    create_tables()
+    uvicorn.run("run:app", host="0.0.0.0", port=8000, reload=True)
