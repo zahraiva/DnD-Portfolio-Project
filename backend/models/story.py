418e1ba22a52bd01a939c6eb056bb6c3787b3a2b
Orxan <9418@holbertonstudents.com>
2025-02-20 13:17:30 +0400

    Change in Story Model

--- backend/models/story.py
+++ backend/models/story.py
@@ -1,11 +1,11 @@
-from sqlalchemy import Column, String
+
+from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 from backend.models.basemodel import BaseModel
 
 class Story(BaseModel):
     __tablename__ = "story"
 
-    title = Column(String, unique=True, index=True)
-    plot = Column(String)
-
-    game_sessions = relationship("GameSession", back_populates="story")
+    title = Column(String(100), unique=True, index=True)
+    plot = Column(String(100))
+    dungeon_master_id = Column(String(36), ForeignKey("dungeon_master.id"), nullable=False)
