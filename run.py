070d47bfd11e561593523c1f8fb58018589a2643
AliiMustafa <aliimustafazada@gmail.com>
2025-03-11 16:30:03 +0400

    Change in pyproject.toml file

--- run.py
+++ run.py
@@ -3,6 +3,9 @@ from fastapi import FastAPI
 from backend.api.v1.auth import router as dungeon_router
 from backend.api.v1.character import router as character_router
 from backend.api.v1.story import router as story_router
+from backend.api.v1.game_character import router as game_character_router
+from backend.api.v1.game_session import router as game_session_router
+from backend.api.v1.character_action import router as character_action_router
 from backend.database import engine, Base
 import uvicorn
 
@@ -11,6 +14,9 @@ app = FastAPI()
 app.include_router(dungeon_router)
 app.include_router(character_router)
 app.include_router(story_router)
+app.include_router(game_session_router)
+app.include_router(game_character_router)
+app.include_router(character_action_router)
 
 async def create_tables():
     async with engine.begin() as conn:
